9ac8a1035ac7729895806c89397d579804933bed
David Adebayo <youremail@example.com>
2025-10-25 20:48:42 +0100

    removed vite plugin terminal

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc"
 import generouted from "@generouted/react-router/plugin"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
-import Terminal from "vite-plugin-terminal"
 import tailwindcss from "@tailwindcss/vite"
 import monicon from "@monicon/vite"
 
@@ -22,11 +21,6 @@ export default defineConfig({
 		generouted({
 			output: "./src/commons/router/router.ts",
 		}),
-		Terminal({
-			//console: "terminal",
-			output: ["console", "terminal"],
-		}),
-
 	],
 	preview: {
 		//@ts-ignore
